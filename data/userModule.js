1c851e3bbdf180bf82e590100c2f1a894fa41d48
alecassera <acassera@udesa.edu.ar>
2021-09-13 20:20:54 -0300

    Datos perfiles 10/10

--- data/userModule.js
+++ data/userModule.js
@@ -39,7 +39,72 @@ const user = {
         foto:"images/winstonChurchillPerfil.jpg",
         fecha:"30/11/1874",
         id: 3
-        }        
+        },        
+
+
+
+        {
+        nombre: "Julio",
+        apellido: "Cesar",
+        usuario: "23StabsMan",
+        email: "JulioCesar@hotmail.com",
+        contrasena: "CaesarRome123",
+        foto:"images/julioCesarPerfil.jpg",
+        fecha:"17/09/80aC",
+        id: 4
+        },
+        {
+        nombre: "Socrates",
+        apellido: "Sofronisco",
+        usuario: "writeLessMind",
+        email: "SocratesSofronisco@atenas.com",
+        contrasena: "MasterOfPlaton",
+        foto:"images/socratesPerfil.jpg",
+        fecha:"02/12/450aC",
+        id: 5
+        },
+        {
+        nombre: "Henry",
+        apellido: "VIII",
+        usuario: "queenSlayer",
+        email: "HenryVIII@outlook.com",
+        contrasena: "KingOfKings",
+        foto:"images/henryViiiPerfil.jpg",
+        fecha:"12/05/1522",
+        id: 6
+        },
+        {
+        nombre: "Ivar",
+        apellido: "Ragnarsson",
+        usuario: "Boneless",
+        email: "RagnarssonB@skol.com",
+        contrasena: "RagnarGod",
+        foto:"images/ivarRagnarssonPerfil.jpg",
+        fecha:"09/09/830dC",
+        id: 7
+        },
+        {
+        nombre: "Diogenes",
+        apellido: "De Sinope",
+        usuario: "homelessBrilliance",
+        email: "DeSinopeDiogenes@hotmail.com",
+        contrasena: "AtenasNewHome",
+        foto:"images/diogenesDeSinopePerfil.jpg",
+        fecha:"28/11/370aC",
+        id: 8
+        },
+        {
+        nombre: "Napoleon",
+        apellido: "Bonaparte",
+        usuario: "NapoleonMandaparte",
+        email: "NapoleonBonaparte@hotmail.com",
+        contrasena: "Greedpower",
+        foto:"images/napoleonBonapartePerfil.jpg",
+        fecha:"12/12/1799",
+        id: 9
+        },  
+
+
     ],
     findUser: function(user){
         for (let i = 0; i < this.lista.length; i++) {
