43c20afa93f13e456595262e6d8a90c8a0ef40f7
PedroBusato <pbusatoader@udesa.edu.ar>
2021-09-21 10:09:52 -0300

    Arregle errores de las imagenes

--- data/posts.js
+++ data/posts.js
@@ -16,7 +16,7 @@ const posts = {
     listaIndex : [
         {
         idPost:0,
-        imagen: 'images/alejandroMagno.png',
+        imagen: '/images/alejandroMagno.png',
         texto: "Discurso en Opis",
         fecha: "324 aC",
         idUsuario:0,
@@ -25,7 +25,7 @@ const posts = {
         },
         {
         idPost:1,
-        imagen: 'images/imagenPrueba2.jpg',
+        imagen: '/images/imagenPrueba2.jpg',
         texto: "Con mi amigo Aristoteles",
         fecha: "317 aC",
         idUsuario: 0,
@@ -34,7 +34,7 @@ const posts = {
         },
         {
         idPost:2,
-        imagen:"images/gengisKan.jpg",
+        imagen:"/images/gengisKan.jpg",
         texto: "Entrando a China con las tropas #DinastiaJin",
         fecha: "1234",
         idUsuario: 2,
@@ -43,7 +43,7 @@ const posts = {
         },
         {
         idPost:3,
-        imagen:"images/winstonChurchill.png",
+        imagen:"/images/winstonChurchill.png",
         texto: "Good ol' times",
         fecha: "1921",
         idUsuario: 3,
@@ -52,7 +52,7 @@ const posts = {
         },
         {
         idPost:4,
-        imagen:"images/imagenPrueba.jpg",
+        imagen:"/images/imagenPrueba.jpg",
         texto: "The greatest ever",
         fecha: "360aC",
         idUsuario: 0,
@@ -61,7 +61,7 @@ const posts = {
         },
         {
         idPost:5,
-        imagen:"images/marcoAurelio.jpg",
+        imagen:"/images/marcoAurelio.jpg",
         texto: "Back from the North",
         fecha: "187",
         idUsuario: 1,
@@ -70,7 +70,7 @@ const posts = {
         },        
         {
         idPost:6,
-        imagen:"images/julioCesarPost.jpg",
+        imagen:"/images/julioCesarPost.jpg",
         texto: "#true #own #thoughts",
         fecha: "79aC",
         idUsuario: 4,
@@ -79,7 +79,7 @@ const posts = {
         },
         {
         idPost:7,
-        imagen:"images/socratesPost.jpg",
+        imagen:"/images/socratesPost.jpg",
         texto: "Tarde de té y dialogo con ellos",
         fecha: "448aC",
         idUsuario: 5,
@@ -88,7 +88,7 @@ const posts = {
         },       
         {
         idPost:8,
-        imagen:"images/henryViiiPost.jpg",
+        imagen:"/images/henryViiiPost.jpg",
         texto: "Mis esposas. Las amo! <3",
         fecha: "1522",
         idUsuario: 6,
@@ -97,7 +97,7 @@ const posts = {
         },
         {
         idPost:9,
-        imagen:"images/ivarRagnarssonPost.jpg",
+        imagen:"/images/ivarRagnarssonPost.jpg",
         texto: "Who needs legs when you can conquer Europe with your mind?",
         fecha: "880dC",
         idUsuario: 7,
@@ -106,7 +106,7 @@ const posts = {
         },
         {
         idPost:10,
-        imagen:"images/diogenesDeSionopePost.jpg",
+        imagen:"/images/diogenesDeSionopePost.jpg",
         texto: "Quien se cree que es este para taparme la luz del sol? #cancelAlexanderTheGreat",
         fecha: "368aC",
         idUsuario: 8,
@@ -115,7 +115,7 @@ const posts = {
         },
         {
         idPost:11,
-        imagen:"images/napoleonBonapartePost.jpg",
+        imagen:"/images/napoleonBonapartePost.jpg",
         texto: "L'avarice casse le sac",
         fecha: "1800",
         idUsuario: 9,
